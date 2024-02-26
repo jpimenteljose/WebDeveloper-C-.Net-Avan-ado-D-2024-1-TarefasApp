ae719f1194ede70477a6f45926f0019213f219df
jpimenteljose <jpimentel.jose@hotmail.com>
2024-02-26 16:39:47 -0300

    first commit

---
+++ TarefasApp.IntegrationTests/TarefasTest.cs
@@ -0,0 +1,34 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace TarefasApp.IntegrationTests
+{
+    /// <summary>
+    /// Classe de integração do ENDPOINT da API de tarefas
+    /// </summary>
+    public class TarefasTest
+    {
+        [Fact(Skip = "Não implementado.")]
+        public async Task PostTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task PutTarefas_ReturnsSuccessStatusCodeAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task DeleteTarefas_ReturnsSuccessStatusCodeAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task GetTarefas_ReturnsSuccessStatusCodeAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
